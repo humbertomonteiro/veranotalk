9f59cac8cb4f7fc4cb8328af1076d5a42ec349bb
Humberto Monteiro <humbertoaraujox@icloud.com>
2025-07-19 06:18:46 -0300

    Configurando RoutesApp React router

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { BrowserRouter } from "react-router-dom";
+import RoutesApp from "./routes/RoutesApp";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <RoutesApp />
+    </BrowserRouter>
+  );
+}
+
+export default App;
