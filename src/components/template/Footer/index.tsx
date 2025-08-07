936166c35610dee7a5b2e18655409dcd5fed985f
Humberto Monteiro <humbertoaraujox@icloud.com>
2025-08-07 11:25:05 -0300

    add função para subir quando clicar e link do meu portfólio no footer

--- src/components/template/Footer/index.tsx
+++ src/components/template/Footer/index.tsx
@@ -2,21 +2,34 @@ import styles from "./footer.module.css";
 import { Link } from "react-router-dom";
 
 export default function Footer() {
+  const toTop = () => {
+    scrollTo(0, 0);
+  };
   return (
     <footer className={styles.footer}>
       <div className={styles.title}>
-        <Link to={"/"}>VERANO TALK</Link>
+        <Link onClick={toTop} to={"/"}>
+          VERANO TALK
+        </Link>
       </div>
       <div className={styles.container}>
         <p className={styles.copyright}>
           © {new Date().getFullYear()} Verano Talk. Todos os direitos
           reservados.
         </p>
-        <Link to={"/privacy-policy"} className={styles.policy}>
+        <Link onClick={toTop} to={"/privacy-policy"} className={styles.policy}>
           Politica de privacidade
         </Link>
         <p className={styles.credits}>
-          Feito por <span className={styles.humdev}>HumDev</span> com 🤍
+          Feito por{" "}
+          <a
+            href="https://portfolio-three-bay-73.vercel.app/"
+            target="_blank"
+            className={styles.humdev}
+          >
+            HumDev
+          </a>{" "}
+          com 🤍
         </p>
       </div>
     </footer>
