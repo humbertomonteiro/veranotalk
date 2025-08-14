4ecda41bc62d2cd4ef24fea4939ddaecbbfcefbd
Humberto Monteiro <humbertoaraujox@icloud.com>
2025-08-14 15:05:22 -0300

    att

--- src/pages/TirarPrint/index.tsx
+++ src/pages/TirarPrint/index.tsx
@@ -2,7 +2,7 @@ import About from "../../components/sections/About";
 import Hero from "../../components/sections/Hero";
 import Highlights from "../../components/sections/Highlights";
 import Location from "../../components/sections/Location";
-import Questions from "../../components/sections/Questions";
+// import Questions from "../../components/sections/Questions";
 import Sponsors from "../../components/sections/Sponsors";
 import styles from "./tirarPrint.module.css";
 
